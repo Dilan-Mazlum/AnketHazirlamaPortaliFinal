8bd298f154b35e347e0ce0976d2ed55452a7e523
Dilan Mazlum <dilanmazlum513@gmail.com>
2022-06-02 10:32:27 +0300

    Add project files.

---
+++ myoBlog29API/ViewModel/YorumModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myoBlog29API.ViewModel
+{
+    public class YorumModel
+    {
+        public int YorumId { get; set; }
+        public string YorumIcerik { get; set; }
+        public int UyeId { get; set; }
+        public int MakaleId { get; set; }
+        public string KullaniciAdi { get; set; }
+        public DateTime Tarih { get; set; }
+    }
+}
